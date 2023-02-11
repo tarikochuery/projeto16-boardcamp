39efa0161467239f6fecac64aead313e953efeff
tarikochuery <ttarikechuery@gmail.com>
2023-02-10 21:27:02 -0300

    FIX: Fix POST /games body request accepting negative values on stockTotal and pricePerDay

--- src/schemas/gameSchema.js
+++ src/schemas/gameSchema.js
@@ -3,6 +3,6 @@ import Joi from "joi";
 export const gameSchema = Joi.object({
   name: Joi.string().required(),
   image: Joi.string(),
-  stockTotal: Joi.number().not(null).required(),
-  pricePerDay: Joi.number().not(null).required()
+  stockTotal: Joi.number().not(null).positive().required(),
+  pricePerDay: Joi.number().not(null).positive().required()
 });
